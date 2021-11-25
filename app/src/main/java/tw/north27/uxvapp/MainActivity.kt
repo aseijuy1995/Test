b5378f6faa9922d6db917659479c4fa7ec6c816d
Yujie <b0972911675@gmail.com>
2021-11-25 11:26:31 +0800

    Sample - Dialog

---
+++ app/src/main/java/tw/north27/uxvapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.north27.uxvapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+		override fun onCreate(savedInstanceState: Bundle?) {
+				super.onCreate(savedInstanceState)
+				setContentView(R.layout.activity_main)
+		}
+}
